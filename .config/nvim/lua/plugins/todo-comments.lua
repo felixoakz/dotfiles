dc6fe1eca07b9ec5cde9a6e7c70f9c10f15cc32a
felixoakz <felix@oakz.org>
2024-09-26 07:55:58 -0300

    updates

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,5 +1,14 @@
 return {
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'BufReadPost',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    },
+    opts = {
+      signs = false
+    }
+  },
 
 }
