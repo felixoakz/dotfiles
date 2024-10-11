b7066d1fe7297a22fb95e2075ab14cf03ebf9b33
felixoakz <felix@oakz.org>
2024-10-10 22:11:00 -0300

    remap recent files to leader leader, waybar configs, activate noice back

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,31 +1,31 @@
 return {
-  -- {
-  --   'folke/noice.nvim',
-  --   event = 'VeryLazy',
-  --   opts = {
-  --     routes = {
-  --       {
-  --         view = 'notify',
-  --         filter = { event = 'msg_showmode' },
-  --       },
-  --     },
-  --     views = {
-  --       cmdline_popup = {
-  --         position = {
-  --           row = -3,
-  --           -- col = -1,
-  --         },
-  --       },
-  --     },
-  --     lsp = {
-  --       signature = {
-  --         enabled = false,
-  --       },
-  --     },
-  --   },
-  --   dependencies = {
-  --     'MunifTanjim/nui.nvim',
-  --     'rcarriga/nvim-notify',
-  --   },
-  -- },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      routes = {
+        {
+          view = 'notify',
+          filter = { event = 'msg_showmode' },
+        },
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = -3,
+            -- col = -1,
+          },
+        },
+      },
+      lsp = {
+        signature = {
+          enabled = false,
+        },
+      },
+    },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+  },
 }
